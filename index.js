7f305f931d4151bbf8fcc09fb1d8149447e0a866
Ryann Anderson <ryannanderson@Ryanns-MacBook-Air.local>
2021-06-24 10:31:59 -0600

    makes it so port works on heroku

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 // This file is in charge of starting the application
 const server = require("./server");
 const persist = require("./persist");
+// const fs = require("fs");
 
 // define a port
-const portNumber = process.argv[2] || 8080;
+const portNumber = process.argv[2]|| process.env.PORT || 8080;
 
 // connect to the database
 persist.connect(function () {
